b3ea596997c32ca1460e76906680006c74343002
Gabriel Silva <gabriel.etefmc@gmail.com>
2020-10-27 12:24:31 -0300

    new methods and type Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear', false));
+    this.todos.push(new Todo(2, 'Jogar', false));
+    this.todos.push(new Todo(3, 'asaaas', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index != -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {}
+
+  markAsUndone() {}
 }
